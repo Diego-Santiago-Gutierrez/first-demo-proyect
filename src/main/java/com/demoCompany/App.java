6d61254d42d5237b27679d10d40cff2e7c5fd3e7
Diego-Santiago-Gutierrez <diego.santiago@solera.com>
2022-08-04 08:14:54 -0500

    Trial message

--- src/main/java/com/demoCompany/App.java
+++ src/main/java/com/demoCompany/App.java
@@ -11,8 +11,9 @@ public class App
     public static void main( String[] args )
     {
         int [] arr = BubbleSort.bubbleSort(new int[]{1, 2, 34, 8, 5, 1, 3, 8, 9, 1});
-
         Arrays.stream(arr).forEach(System.out::println);
+
+        System.out.println("This message is a trial");
 
     }
 }
