9aa178a64773c947c5412a68e4ef3f1cf7769f7d
Diego-Santiago-Gutierrez <diego.santiago@solera.com>
2022-08-04 08:12:26 -0500

    Add a sort algorithm (BubbleSort)

--- src/main/java/com/demoCompany/App.java
+++ src/main/java/com/demoCompany/App.java
@@ -1,5 +1,7 @@
 package com.demoCompany;
 
+import java.util.Arrays;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        int [] arr = BubbleSort.bubbleSort(new int[]{1, 2, 34, 8, 5, 1, 3, 8, 9, 1});
+
+        Arrays.stream(arr).forEach(System.out::println);
+
     }
 }
